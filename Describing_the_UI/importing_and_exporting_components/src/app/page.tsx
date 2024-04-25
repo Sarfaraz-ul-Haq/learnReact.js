c10d1060adc41716ea2da14041b514d7277cca1f
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-04-25 13:07:42 +0500

    initialize next.js project

---
+++ Describing_the_UI/importing_and_exporting_components/src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Importing and Exporting Components</h1>
+      <div>
+        The magic of components lies in their reusability: you can create
+        components that are composed of other components. But as you nest more
+        and more components, it often makes sense to start splitting them into
+        different files. This lets you keep your files easy to scan and reuse
+        components in more places.
+      </div>
+    </div>
+  );
+}
